7d05beadde7ee8d36c8d457fca711d32c2a41bee
Shahrukh <hafeedhasan110@gmail.com>
2022-11-08 08:41:04 +0530

    added profile

--- realestate/urls.py
+++ realestate/urls.py
@@ -6,7 +6,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
- ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+ ]
+#  + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
 
