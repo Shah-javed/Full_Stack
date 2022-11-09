85330627f836b9e9250001ca9a63eb7bda2e21b8
Shahrukh <hafeedhasan110@gmail.com>
2022-11-09 16:24:48 +0530

    crud-functionality

---
+++ apps/enquires/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Enquiry
+
+
+class EnquiryAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "phone_number", "message"]
+
+
+admin.site.register(Enquiry, EnquiryAdmin)
